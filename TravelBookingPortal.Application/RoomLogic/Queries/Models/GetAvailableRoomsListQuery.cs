7e68cee8e2c1509a4d043448bb0ca383aa66f8b8
rehab-megahed123 <rehabmegahed241@gmail.com>
2025-04-09 23:21:39 +0200

    update filtering feature

--- TravelBookingPortal.Application/RoomLogic/Queries/Models/GetAvailableRoomsListQuery.cs
+++ TravelBookingPortal.Application/RoomLogic/Queries/Models/GetAvailableRoomsListQuery.cs
@@ -6,6 +6,7 @@ namespace TravelBookingPortal.Application.RoomLogic.Queries.Models
 {
    public class GetAvailableRoomsListQuery : IRequest<IEnumerable<GetRoomsDTO>>
     {
+        
         public string City { get; set; }
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
