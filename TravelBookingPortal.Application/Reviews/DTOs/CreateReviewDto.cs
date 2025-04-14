4f99fff6936f835b4a618dee8d356995b5912e71
hananhamed10 <hananhamty@gmail.com>
2025-04-14 22:25:42 +0200

    Finish 1  of end point of Review

---
+++ TravelBookingPortal.Application/Reviews/DTOs/CreateReviewDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBookingPortal.Application.Reviews.DTOs
+{
+    internal class CreateReviewDto
+    {
+        public string UserId { get; set; } = null!;
+        public int HotelId { get; set; }
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+    }
+}
---
+++ TravelBookingPortal.Infrastructure/Repositories/ReviewRepo/ReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TravelBookingPortal.Domain.Enitites.ReviewEntities;
+using TravelBookingPortal.Domain.Repositories.ReviewRepo;
+using TravelBookingPortal.Infrastructure.DbContext;
+
+namespace TravelBookingPortal.Infrastructure.Repositories.ReviewRepo
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly TravelBookingPortalDbContext context;
+
+        public ReviewRepository(TravelBookingPortalDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task AddAsync(Review review)
+        {
+            context.Reviews.Add(review);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<Review>> GetByUserIdAsync(string userId)
+        {
+            return await context.Reviews.Where(U=>U.UserId == userId).ToListAsync();
+        }
+    }
+}
