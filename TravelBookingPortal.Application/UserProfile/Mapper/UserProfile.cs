9f1b3e2b061533deacccf27cef025fbdf26cec9e
marwan abed <marwanabed453@gmail.com>
2025-04-20 23:04:49 +0200

    Adding mapper

---
+++ TravelBookingPortal.Application/UserProfile/Mapper/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿
+
+using AutoMapper;
+using TravelBookingPortal.Application.UserProfile.Dtos;
+using TravelBookingPortal.Domain.Entites.User;
+
+namespace TravelBookingPortal.Application.UserProfile.Mapper
+{
+    public class UserProfile : Profile
+    {
+
+        public UserProfile() 
+        {
+            CreateMap<ApplicationUser, UserProfileDto>()
+             .ForMember(p => p.Email, opt => opt.MapFrom(src => src.Email))
+             .ForMember(p => p.UserName, opt => opt.MapFrom(src => src.UserName));
+        }
+    }
+}
